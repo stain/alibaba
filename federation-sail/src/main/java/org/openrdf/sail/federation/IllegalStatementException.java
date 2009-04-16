f7b6da06ddf6e4d4661dbf5a2ef03a55b96bd3b0
james_leigh <james_leigh@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-04-16 12:01:24 +0000

    Remove QueryModelSerializer
    git-svn-id: http://repo.aduna-software.org/svn/org.openrdf/alibaba/trunk@9228 3fbd4d3e-0f96-47c4-bab7-89622e02a19e

--- federation-sail/src/main/java/org/openrdf/sail/federation/IllegalStatementException.java
+++ federation-sail/src/main/java/org/openrdf/sail/federation/IllegalStatementException.java
@@ -4,6 +4,8 @@ import org.openrdf.sail.SailException;
 
 public class IllegalStatementException extends SailException {
 
+	private static final long serialVersionUID = 3653794267690820761L;
+
 	public IllegalStatementException(String string) {
 		super(string);
 	}
