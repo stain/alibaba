0aa71561e7dcff55d01cb23b177864de812c4605
james_leigh <james_leigh@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2011-02-09 14:22:11 +0000

    Add Authentication Vary test
    git-svn-id: http://repo.aduna-software.org/svn/org.openrdf/alibaba/trunk@10799 3fbd4d3e-0f96-47c4-bab7-89622e02a19e

--- object-server/src/test/java/org/openrdf/http/object/AuthenticationTest.java
+++ object-server/src/test/java/org/openrdf/http/object/AuthenticationTest.java
@@ -6,6 +6,7 @@ import org.apache.http.HttpMessage;
 import org.apache.http.HttpResponse;
 import org.apache.http.message.BasicHttpRequest;
 import org.openrdf.http.object.annotations.cacheControl;
+import org.openrdf.http.object.annotations.header;
 import org.openrdf.http.object.annotations.method;
 import org.openrdf.http.object.annotations.realm;
 import org.openrdf.http.object.annotations.type;
@@ -38,7 +39,8 @@ public class AuthenticationTest extends MetadataServerTestCase {
 
 		public Object authenticateRequest(String method, Object resource,
 				Map<String, String[]> request) throws RepositoryException {
-			if (request.containsKey("authorization") && !"bad".equals(request.get("authorization")[0]))
+			if (request.containsKey("authorization")
+					&& !"bad".equals(request.get("authorization")[0]))
 				return "me";
 			return null;
 		}
@@ -51,9 +53,8 @@ public class AuthenticationTest extends MetadataServerTestCase {
 		public HttpMessage authenticationInfo(String method, Object resource,
 				Map<String, String[]> request) {
 			BasicHttpRequest msg = new BasicHttpRequest("GET", "/");
-			msg
-					.addHeader("Authentication-Info", request
-							.get("authorization")[0]);
+			msg.addHeader("Authentication-Info",
+					request.get("authorization")[0]);
 			return msg;
 		}
 
@@ -82,7 +83,25 @@ public class AuthenticationTest extends MetadataServerTestCase {
 		}
 	}
 
+	@iri("urn:test:MyResource")
+	public static class MyResource {
+
+		@method("GET")
+		@type("text/plain")
+		public String get(@header("Authorization") String auth) {
+			return auth;
+		}
+
+		@method("POST")
+		@type("text/plain")
+		public String post(@type("text/plain") String input,
+				@header("Authorization") String auth) {
+			return auth;
+		}
+	}
+
 	public void setUp() throws Exception {
+		config.addConcept(MyResource.class);
 		config.addConcept(MyProtectedResource.class);
 		config.addConcept(MyRealm.class);
 		super.setUp();
@@ -90,6 +109,8 @@ public class AuthenticationTest extends MetadataServerTestCase {
 		try {
 			String uri = client.path("/protected").getURI().toASCIIString();
 			con.addDesignation(con.getObject(uri), MyProtectedResource.class);
+			uri = client.path("/resource").getURI().toASCIIString();
+			con.addDesignation(con.getObject(uri), MyResource.class);
 		} finally {
 			con.close();
 		}
@@ -113,7 +134,8 @@ public class AuthenticationTest extends MetadataServerTestCase {
 		MyProtectedResource.body = "second";
 		resp = web.header("Authorization", "two").get(ClientResponse.class);
 		assertNotNull(resp.getHeaders().get("ETag"));
-		assertEquals("first", resp.getEntity(String.class)); // body should be cached
+		// body should be cached
+		assertEquals("first", resp.getEntity(String.class));
 		assertEquals("two", resp.getHeaders().get("Authentication-Info").get(0));
 	}
 
@@ -136,8 +158,29 @@ public class AuthenticationTest extends MetadataServerTestCase {
 		MyProtectedResource.body = "second";
 		resp = web.header("Authorization", "two").get(ClientResponse.class);
 		assertNotNull(resp.getHeaders().get("ETag"));
-		assertEquals("first", resp.getEntity(String.class)); // body should still be cached
+		// body should still be cached
+		assertEquals("first", resp.getEntity(String.class));
 		assertEquals("two", resp.getHeaders().get("Authentication-Info").get(0));
+	}
+
+	public void testPostVaryAuth() throws Exception {
+		ClientResponse resp;
+		WebResource web = client.path("/resource");
+		resp = web.header("Authorization", "test").post(ClientResponse.class, "input");
+		assertEquals("test", resp.getEntity(String.class));
+		assertNull(resp.getHeaders().get("Cache-Control"));
+		assertNotNull(resp.getHeaders().get("ETag"));
+		assertFalse(resp.getHeaders().get("Vary").toString().contains("Authorization"));
+	}
+
+	public void testGetVaryAuth() throws Exception {
+		ClientResponse resp;
+		WebResource web = client.path("/resource");
+		resp = web.header("Authorization", "test").get(ClientResponse.class);
+		assertEquals("test", resp.getEntity(String.class));
+		assertTrue(resp.getHeaders().get("Cache-Control").get(0).contains("private"));
+		assertNotNull(resp.getHeaders().get("ETag"));
+		assertFalse(resp.getHeaders().get("Vary").toString().contains("Authorization"));
 	}
 
 }
